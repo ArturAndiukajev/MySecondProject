e7a1f69f19bc0b27cf08a7769b0f15c7e38f4aad
ArturAndiukajev <144611797+ArturAndiukajev@users.noreply.github.com>
2023-09-20 19:36:46 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 using namespace std;
 
 
@@ -9,7 +10,18 @@ struct Studentas{
     string pavarde;
     vector <int> ND;
     int Egz;
+    double galutinis;
 };
+//------------------------
+
+//------------------------
+double rezultatas(double suma,int kiekis,int egz)
+{
+    double rez=0;
+    rez=0.4*(suma/kiekis)+0.6*egz;
+    return rez;
+}
+
 int main()
 {
     int stud_sk;
@@ -28,15 +40,28 @@ int main()
         int nd_kiekis;
         cout<<"Iveskite "<<i+1<<" studento namu darbu kieki"<<endl;
         cin>>nd_kiekis;
+        double nd_suma=0;
         for(int j=0;j<nd_kiekis;j++)
         {
             int nd_rez;
             cout<<"Iveskite "<<i+1<<" studento"<<j+1<<" namu darbo rezultata"<<endl;
             cin>>nd_rez;
             naujas_st.ND.push_back(nd_rez);
+            nd_suma=nd_suma+nd_rez;
         }
         cout<<"Iveskite "<<i+1<<" studento egzamino rezultata"<<endl;
         cin>>naujas_st.Egz;
+        naujas_st.galutinis=rezultatas(nd_suma, nd_kiekis, naujas_st.Egz);
+
+        studentai.push_back(naujas_st);
+    }
+
+    //Duomenu isvedimas
+    cout<<"Vardas"<<setw(10)<<"Pavarde"<<setw(12)<<"Galutinis"<<endl;
+    cout<<"-------------------------------------------------------------------------------"<<endl;
+    for(int i=0;i<stud_sk;i++)
+    {
+        cout<<studentai[i].vardas<<setw(10)<<studentai[i].pavarde<<setw(12)<<fixed<<setprecision(2)<<studentai[i].galutinis<<endl;
     }
     return 0;
 }
