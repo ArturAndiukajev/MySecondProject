c7b53884f4b360658e76e03d80b08c6416769ac2
ArturAndiukajev <144611797+ArturAndiukajev@users.noreply.github.com>
2023-09-20 13:37:39 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+struct Mokinys{
+    string vardas;
+    string pavarde;
+    vector <int> ND;
+    int Egz;
+};
+int main()
+{
+
+    return 0;
+}
