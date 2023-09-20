8409a134983e0b0e53a50e4917c6d55723b89f1f
ArturAndiukajev <144611797+ArturAndiukajev@users.noreply.github.com>
2023-09-20 18:48:22 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,17 +17,26 @@ int main()
 
     cout<<"Iveskite studentu kieki: "<<endl;
     cin>>stud_sk;
-
+    //Duomenu ivedimas
     for(int i=0;i<stud_sk;i++)
     {
+        Studentas naujas_st;
         cout<<"Iveskite "<<i+1<<" studento varda"<<endl;
-        cin>>studentai.vardas;
+        cin>>naujas_st.vardas;
         cout<<"Iveskite "<<i+1<<" studento pavarde"<<endl;
-        cin>>studentai.pavarde;
+        cin>>naujas_st.pavarde;
         int nd_kiekis;
         cout<<"Iveskite "<<i+1<<" studento namu darbu kieki"<<endl;
         cin>>nd_kiekis;
-
+        for(int j=0;j<nd_kiekis;j++)
+        {
+            int nd_rez;
+            cout<<"Iveskite "<<i+1<<" studento"<<j+1<<" namu darbo rezultata"<<endl;
+            cin>>nd_rez;
+            naujas_st.ND.push_back(nd_rez);
+        }
+        cout<<"Iveskite "<<i+1<<" studento egzamino rezultata"<<endl;
+        cin>>naujas_st.Egz;
     }
     return 0;
 }
