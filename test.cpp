ea5870576dda7ad5093e9feed314e53912103fac
ArturAndiukajev <144611797+ArturAndiukajev@users.noreply.github.com>
2023-12-21 21:08:06 +0200

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
+#include "firstlib.h"
 #include "firstlib.cpp"
-#include "firslib.h"
+
 
 
 TEST(KlasesStudentTestas, StudentoVeikymoPatykra ) 
@@ -14,32 +15,15 @@ TEST(KlasesStudentTestas, StudentoVeikymoPatykra )
     studentas.setEgzaminas(10);
     EXPECT_EQ("Cipsas", studentas.getVardas());
     EXPECT_EQ("Kebabas", studentas.getPavarde());
-    EXPECT_EQ(10, studentas.getEgzaminas());
+    EXPECT_EQ(10, studentas.getEgz());
 }
 
-TEST(KlasesStudentTestas, StudentoPatykra)
+TEST(KlasesStudentTestas, StudentoPazymiuPatykra)
 {
     Studentas studentas;
-    studentas.setVardas("Cipsas");
-    studentas.setPavarde("Kebabas");
     studentas.addND(-5);
     studentas.addND(10);
     studentas.addND(33);
-    studentas.setEgzaminas(10);
-    EXPECT_EQ("Cipsas", studentas.vardas());
-    EXPECT_EQ("Kebabas", studentas.pavarde());
-    EXPECT_EQ(10, studentas.egzaminas());
     EXPECT_THROW(studentas.addND(-5), std::invalid_argument);
-    EXPECT_EQ("Pazymys negali buti didesnis uz 10 arba mazesnis uz 0");
-}
-TEST(KlasesStudentTestas, StudentoRezultatuPatykra)
-{
-    Studentas studentas;
-    studentas.addND(5);
-    studentas.addND(8);
-    studentas.addND(7);
-    studentas.setEgzaminas(9);
-    EXPECT_DOUBLE_EQ(7.4, studentas.getGalutinis());
-    double mediana = studentas.Med();
-    EXPECT_DOUBLE_EQ(8.6, studentas.rezMed(mediana));
+    EXPECT_THROW(studentas.addND(33), std::invalid_argument);
 }
